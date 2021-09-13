fc53b651f9ff45b75e74f1cbc79fdc9be84f9a38
Liam <liamhoughton90@yahoo.com>
2021-09-13 13:52:55 -0500

    first prompt done

--- lab1.c
+++ lab1.c
@@ -2,9 +2,26 @@
 
 
 
+int setUp();
+
 
 int main(int argc, char *argv[]){
+	int input = setUp();
+	printf("%d", input);
 
+
+}
+
+
+int setUp(){
+	int input;
+	printf("Enter the number and the following will happen:\n ");
+	printf("1 - Add 2 numbers \n");
+	printf("2 - Multiply 2 numbers \n");
+	printf("3 - Read in string and reverse the string as output.\n");
+	printf("4 - Output the current date and time\n");
+	scanf("%d", &input);
+       	return input;	
 
 
 
